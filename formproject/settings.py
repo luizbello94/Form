2be8ec27b99776a9c2ca51acca85d5cb1265cb75
Luis <luisernestoperezbello@gmail.com>
2023-10-17 10:33:15 -0600

    requirements

--- formproject/settings.py
+++ formproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@k(ki&n3sik^*j%vh-yr@sm-d@!l7^mn!)2mgk+-e&=czf&q=a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
